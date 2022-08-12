5bfb4da89f445f9dc627493749d94dd23b1fac84
KAHA <wannasaynone@hotmail.com>
2022-08-13 00:42:20 +0800

    add tank

---
+++ Assets/Tests/TankTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace ProjectTank.Test
+{
+    public class TankTest
+    {
+        [Test]
+        public void create()
+        {
+            UnityEngine.Vector3 startPosition = UnityEngine.Vector3.zero;
+            int startHP = 100;
+            int startAttack = 10;
+
+            Tank tankInstance = new Tank(startPosition, startHP, startAttack);
+
+            Assert.AreEqual(startPosition, tankInstance.CurrentStats.Position);
+            Assert.AreEqual(startHP, tankInstance.CurrentStats.HP);
+            Assert.AreEqual(startAttack, tankInstance.CurrentStats.Attack);
+        }
+
+
+    }
+}
